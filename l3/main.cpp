948acdd2e765a3a8d8936ba154845f08131c0571
ScottPiligrim <127794151+ScottPiligrim@users.noreply.github.com>
2024-10-11 18:21:01 +0200

    Add files via upload

---
+++ l3/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, i;
+    cout << "Ввести x (0<x<10^9) и i (0<=i<=31): ";
+    cin >> x >> i;
+    if (x<=0 || x>=1000000000) {
+        cout << "Ошибк." << endl;
+    }
+    else if (i<0 || i>31) {
+        cout << "Ошибк." << endl;
+    }
+    else {
+        cout << ((x >> i) & 1) << endl;
+    }
+    return 0;
+}
