52874747d5142906901fd2f6c39aa42b6e5751ee
ScottPiligrim <127794151+ScottPiligrim@users.noreply.github.com>
2024-10-12 10:40:45 +0200

    Update main.cpp

--- l4/main.cpp
+++ l4/main.cpp
@@ -15,7 +15,7 @@ using namespace std;
 int main()
 {
     int a, b, c;
-    cout << "Введите натуральные числа A, B и C: ";
+    cout << "Enter natural numbers A, B and C: ";
     cin >> a >> b >> c;
     if (a>b & b>c) {
         cout << (b/c+b-a) << endl;
@@ -26,20 +26,20 @@ int main()
 
 
     int N;
-    cout << "Введите число N (0<=N<=9): ";
+    cout << "Enter N (0<=N<=9): ";
     cin >> N;
     switch (N) {
-        case 0: cout << "Ноль" << endl; break;
-        case 1: cout << "Один" << endl; break;
-        case 2: cout << "Два" << endl; break;
-        case 3: cout << "Три" << endl; break;
-        case 4: cout << "Четыре" << endl; break;
+        case 0: cout << "Zero" << endl; break;
+        case 1: cout << "One" << endl; break;
+        case 2: cout << "Two" << endl; break;
+        case 3: cout << "Three" << endl; break;
+        case 4: cout << "Four" << endl; break;
         case 5: cout << "Five" << endl; break;
-        case 6: cout << "Шесть" << endl; break;
-        case 7: cout << "Семь" << endl; break;
-        case 8: cout << "Восемь" << endl; break;
-        case 9: cout << "Девять" << endl; break;
-        default: cout << "Ошибк" << endl;
+        case 6: cout << "Six" << endl; break;
+        case 7: cout << "Seven" << endl; break;
+        case 8: cout << "Eight" << endl; break;
+        case 9: cout << "Nine" << endl; break;
+        default: cout << "Err" << endl;
     }
 
 
